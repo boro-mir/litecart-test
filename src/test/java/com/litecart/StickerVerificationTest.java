ebe87283d88d188b3f2630042b2f76daf697f8fa
Boris <sqaboris@gmail.com>
2018-11-07 00:06:50 -0500

    task 8 refactor

--- src/test/java/com/litecart/StickerVerificationTest.java
+++ src/test/java/com/litecart/StickerVerificationTest.java
@@ -1,11 +1,8 @@
 package com.litecart;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,12 +10,10 @@ public class StickerVerificationTest {
 
     @Test
     public void testVerifyStickerIsPresent() {
-        ElementsCollection allItems = $$(".products .image-wrapper");
-        Configuration.browser = "chrome";
+        ElementsCollection allItems = $$(".products .link");
+        ElementsCollection allStickers = $$(".products .image-wrapper > .sticker");
         open("http://localhost/litecart/en/");
-        for (SelenideElement item : allItems) {
-            item.shouldHave(attribute("title"));
-        }
+        allItems.shouldHaveSize(allStickers.size());
     }
 
 }
